d5ea5ba300a6b2c4cf7ec55c5782896bd85ccc4d
Patryk <patryknowak921@gmail.com>
2017-05-07 20:24:43 +0200

    dupa

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
 var name = prompt('Wpisz swoje imię'); //funkcja odpowiada za wyświetlenie pierwszego okienka pytaniem o imię oraz polem do wpisania wartości
-alert('Witaj, ' + name); //wyświetla wartość w okienku wyskakującym w przeglądarce
+//alert('Witaj, ' + name);
 console.log('Witaj, ' + name); //jak wyżej - z tym, że wartość zostanie wyświetlona w narzędziach deweloperskich
